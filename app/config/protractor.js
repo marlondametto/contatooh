25aa8105aa49d9f643109df1b6f587e52895ed42
marlondametto <marlondametto@gmail.com>
2015-08-16 21:33:06 -0300

    commit da secao do Sauce Labs

--- app/config/protractor.js
+++ app/config/protractor.js
@@ -2,7 +2,19 @@
 
 var config = require('./config')();
 
+
 exports.config = {
+	sauceUser: config.sauceUser,
+	sauceKey: config.sauceKey,
+	capabilities: {
+		'name': config.sauceTestName,
+		'browserName': 'chrome',
+		'tunnel-identifier': config.travisJobNumber,
+		'build': config.travisBuild
+	},
+
+
+
 	specs: ['../../test/e2e/**/*.js'],
 	onPrepare: function() {
 		browser.get('http://localhost:3000');
